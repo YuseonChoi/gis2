9a74c6285e22c04de07916de9db9eaf0e8c34559
sunny <chldbtjs74@gmail.com>
2021-07-14 10:25:04 +0900

    7/14 class2 commit 2

--- gis2/settings.py
+++ gis2/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -151,4 +151,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accoutapp:login')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
